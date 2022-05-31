5177755e4a55c13f4edc6e1ff3092ace806bedb2
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2022-05-31 07:57:06 -0300

    feature: init project

---
+++ java-sales-system/src/main/java/com/mycompany/javasalessystem/Mocks/ProductMock.java
@@ -0,0 +1,5 @@
+package com.mycompany.javasalessystem.Mocks;
+
+public class ProductMock {
+    
+}
---
+++ java-sales-system/src/main/java/com/mycompany/javasalessystem/Repositories/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.mycompany.javasalessystem.Repositories;
+
+public class ProductRepository {
+    
+}
